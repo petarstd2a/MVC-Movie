3249e0e3b60e6c8cb1ab02361192495ce0505137
petarstd2a <just_peter_98@mail.bg>
2018-11-29 18:07:49 +0200

    first commit

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public decimal Price { get; set; }  
+    }
+}
---
+++ MvcMovie/Models/OfficeAssignment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class OfficeAssignment
+    {
+        [Key]
+        public int ID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+
+        public Movie Instructor { get; set; }
+    }
+}
